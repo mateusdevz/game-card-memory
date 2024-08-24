1379bd77ed7bf51b07e92ef4fd31b404c5692c72
mhscosta <mhscosta@tjba.jus.br>
2024-08-24 01:27:14 -0300

    Monta grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,117 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'game-card-memory';
+
+  cards = [
+    {
+      id: 1,
+      text: 'Mateus',
+      selected: false
+    },
+    {
+      id: 1,
+      text: 'Mateus',
+      selected: false
+    },
+    {
+      id: 2,
+      text: 'Geovanna',
+      selected: false
+    },
+    {
+      id: 2,
+      text: 'Geovanna',
+      selected: false
+    },
+    {
+      id: 3,
+      text: 'Karol',
+      selected: false
+    },
+    {
+      id: 3,
+      text: 'Karol',
+      selected: false
+    },
+    {
+      id: 4,
+      text: 'Camile',
+      selected: false
+    },
+    {
+      id: 4,
+      text: 'Camile',
+      selected: false
+    },
+    {
+      id: 5,
+      text: 'Renata',
+      selected: false
+    },
+    {
+      id: 5,
+      text: 'Renata',
+      selected: false
+    },
+
+    {
+      id: 6,
+      text: 'Mauricio',
+      selected: false
+    },
+    {
+      id: 6,
+      text: 'Mauricio',
+      selected: false
+    },
+
+    {
+      id: 7,
+      text: 'Edileuza',
+      selected: false
+    },
+    {
+      id: 7,
+      text: 'Edileuza',
+      selected: false
+    },
+
+    {
+      id: 8,
+      text: 'Daniel',
+      selected: false
+    },
+    {
+      id: 8,
+      text: 'Daniel',
+      selected: false
+    },
+
+    {
+      id: 9,
+      text: 'Neusa',
+      selected: false
+    },
+    {
+      id: 9,
+      text: 'Neusa',
+      selected: false
+    },
+    {
+      id: 10,
+      text: 'Jair',
+      selected: false
+    },
+    {
+      id: 10,
+      text: 'Jair',
+      selected: false
+    },
+  ];
+
+  handleClick(card: any): void {
+    card.selected = true;
+    console.log('---- ', card);
+
+  }
 }
